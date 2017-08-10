24edf18c294470e1504262c34ff8af78798608df
Mike Dacanay <migd64@gmail.com>
2017-08-09 21:00:50 -0700

    finished project, formatted the date to seperate year from month and day

--- server.py
+++ server.py
@@ -1,16 +1,27 @@
 from flask import Flask, request, render_template, redirect
+from mysqlconnection import MySQLConnector
 app = Flask(__name__)
+
+mysql = MySQLConnector(app, 'friends')
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	friends = mysql.query_db("SELECT name, age, DATE_FORMAT(c_date, '%M %D') AS c_date, DATE_FORMAT(c_date, '%Y') AS c_date2 FROM users")
+	print friends
+	return render_template('index.html',all_friends=friends)
 
 @app.route('/more', methods=['POST'])
 def add():
-	name=request.form['name1']
-	age=request.form['age1']
-	# print name///IT WORKS IT CONNECTS
-	# print age///IT WORKS IT CONNECTS
+	query="INSERT INTO users(name,age,c_date,u_date) VALUES(:name2,:age2,NOW(),NOW())"
+
+	data= {
+			'name2': request.form['name1'],
+			'age2': request.form['age1']
+		}
+
+	mysql.query_db(query, data)
 	return redirect('/')
 
 app.run(debug=True)
+
+# DATE_FORMAT(c_date, "%M %D") 
